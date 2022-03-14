38e2f32d667f9f34c9e17e79f1fd04133a59706d
akhilesh0598 <akhilesh.chaurasiya0598@gmail.com>
2022-03-14 12:32:12 +0530

    C# fundamental Assignment 1

---
+++ CSharpAssignment1/Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment1
+{
+    public delegate void EmployeeDetailsDelegate(object sender, MessageEventArgs eventArgs);
+
+    public class MessageEventArgs:EventArgs
+    {
+        public string Message { get; set; }
+    }
+    class Employee
+    {
+        
+        public event EmployeeDetailsDelegate DetailsDelegate;
+        private int _id;
+        private string _name;
+        private string _departmentName;
+        
+        public Employee(int id,string name,string departmentName)
+        {
+            _id = id;
+            _name = name;
+            _departmentName = departmentName;
+        }
+        public int GetId()
+        {
+            string message = "GetId() method called";
+            OnDetailsDelegate(message);
+            return _id;
+        }
+        public string GetName()
+        {
+            string message = "GetName() method called";
+            OnDetailsDelegate(message);
+            return _name;
+        }
+        public string GetDepartmentName()
+        {
+            string message = "GetDepartmentName() method called";
+            OnDetailsDelegate(message);
+            return _departmentName;
+        }
+        public void UpdateEmployeeDetail(int id)
+        {
+            _id = id;
+        }
+        public void UpdateEmployeeDetail(int id,string name)
+        {
+            _name = name;
+        }
+        public void UpdateEmployeeDetail(int id,string name,string departmentName)
+        {
+            _id = id;
+            _name = name;
+            _departmentName = departmentName;
+           
+        }
+        protected virtual void OnDetailsDelegate(string message)
+        {
+            if(DetailsDelegate!=null)
+            {
+                DetailsDelegate(this, new MessageEventArgs() { Message = message });
+            }
+
+        }
+    }
+}
---
+++ CSharpAssignment1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharpAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 0;
+            bool isIdCorrect = false;
+            do
+            {
+                Console.WriteLine("Enter the Employee Id  ");
+                isIdCorrect = int.TryParse(Console.ReadLine(), out id);
+                if(!isIdCorrect)
+                {
+                    Console.WriteLine("Please Provide Id in Integer!");
+                }
+            } while (!isIdCorrect);
+
+            Console.WriteLine("Enter the Employee Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the Employee Department Name");
+            string departmentName = Console.ReadLine();
+            Employee employee = new Employee(id, name, departmentName);
+
+            employee.DetailsDelegate += EmployeeMethodCalled;
+
+            Console.WriteLine("Employee Details are - ");
+            Console.WriteLine($"  Id - {employee.GetId()}");
+            Console.WriteLine($"  Name - {employee.GetName()}");
+            Console.WriteLine($"  Department Name - {employee.GetDepartmentName()}");
+
+        }
+        public static void EmployeeMethodCalled(object sender, MessageEventArgs messageEventArgs)
+        {
+            Console.WriteLine(messageEventArgs.Message);
+        }
+        
+    }
+}
