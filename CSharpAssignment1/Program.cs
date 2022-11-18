5be8615e4c3ab31f0adbb09ee30d0f6bcb6e4a73
Akhilesh Chaurasiya <akhilesh.chaurasiya0598@gmail.com>
2022-11-18 12:04:38 +0530

    Adds 2

--- CSharpAssignment1/Program.cs
+++ CSharpAssignment1/Program.cs
@@ -6,36 +6,10 @@ namespace CSharpAssignment1
     {
         static void Main(string[] args)
         {
-            int id = 0;
-            bool isIdCorrect = false;
-            do
-            {
-                Console.WriteLine("Enter the Employee Id  ");
-                isIdCorrect = int.TryParse(Console.ReadLine(), out id);
-                if(!isIdCorrect)
-                {
-                    Console.WriteLine("Please Provide Id in Integer!");
-                }
-            } while (!isIdCorrect);
-
-            Console.WriteLine("Enter the Employee Name");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter the Employee Department Name");
-            string departmentName = Console.ReadLine();
-            Employee employee = new Employee(id, name, departmentName);
-
-            employee.DetailsDelegate += EmployeeMethodCalled;
-
-            Console.WriteLine("Employee Details are - ");
-            Console.WriteLine($"  Id - {employee.GetId()}");
-            Console.WriteLine($"  Name - {employee.GetName()}");
-            Console.WriteLine($"  Department Name - {employee.GetDepartmentName()}");
+            Console.WriteLine("Hi I am akhilesh");
 
         }
-        public static void EmployeeMethodCalled(object sender, MessageEventArgs messageEventArgs)
-        {
-            Console.WriteLine(messageEventArgs.Message);
-        }
-        
+
+
     }
 }
